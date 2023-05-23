718f09d531b133b27edbb422f4034e88b38cc01d
ONJoseph <on.josephinfo@gmail.com>
2023-05-23 13:43:44 +0100

    Add components call to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./redux/configureStore";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
