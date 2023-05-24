ed8d237c964a56600a98cbf4fd4692c726e1e130
ONJoseph <on.josephinfo@gmail.com>
2023-05-24 08:06:23 +0100

    fix import typo error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,13 @@ import './controllers';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './react/redux/configureStore';
+import store from './react/redux/store';
 import App from './react/components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider configureStore={configureStore}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
